b03a8c0a765324bee9f8f9b7cbdeec190309aa3b
Marco <mfseman@umich.edu>
2019-03-31 13:44:30 -0400

    Adding split paragraph to sentence
    Splits a paragraph string into individual sentences for testing purposes. This only for testing as of now.

--- NltkStuff.py
+++ NltkStuff.py
@@ -2,9 +2,10 @@ import nltk, re, pprint
 from nltk import word_tokenize
 from urllib import request
 from bs4 import BeautifulSoup
-from nltk.tokenize import PunktSentenceTokenizer
+from nltk.tokenize import PunktSentenceTokenizer, sent_tokenize
 from tkinter import *
 from database import database
+from nltk.corpus import gutenberg
 import os
 
 #Menu to help user navigate through program.
@@ -17,12 +18,13 @@ def Main_Menu():
     print("---------------------------------------------------------")
 
 
-#sub menu for user to choose (loca/web access)
+#sub menu for user to choose (local/web access)
 def minor_menu():
     user_choice= ""
     #while loop
     print("1. Access local")
     print("2. Access Web")
+    print("3. Book (Moby Dick)")
     
     user_choice= input("Enter choice: ")
 
@@ -33,10 +35,22 @@ def minor_menu():
     elif(user_choice=="2"):
         if(read_from_web()=="1"):
             print("SUCCESSS!")
+
+    elif(user_choice=="3"):
+        if(read_from_book() == "3"):
+            print("SUCCESS!")
         
     else:
         #this will be the code if user enters invalid answer, it will take them to main menu
         return "000"
+
+#Reads from inputted txt file
+def read_from_book():
+    raw = "is an 1851 novel by American writer Herman Melville. The book is sailor Ishmael's narrative of the obsessive quest of Ahab captain of the whaling ship Pequod for revenge on Moby Dick. the white whale that on the ship's previous voyage bit off Ahab's. leg at the knee. A contribution to the literature of the. American Renaissance. the work's genre classifications range from late Romantic to early Symbolist. Moby-Dick was published to mixed reviews was a commercial. failure and was out of print at the time of the. author's death in 1891. Its reputation as a Great American Novel was established only in the 20th century, after the centennial of its author's birth. William Faulkner confessed he wished he had written the book himself"
+    raw2 = sent_tokenize(raw)
+    print(raw2)
+    
+
 
 #Read from local directory
 def read_text():
@@ -57,9 +71,12 @@ def read_text():
     # Read from file
     raw=f.read()
     
-    #Passing value by reference
+    #Passing value by reference (goes to print_tokenizer) and passes the data there as well
     print_tokenizer(raw)
-
+    
+    #Passing values by references (goes to print sentence) and passes data there
+    #print_sentence(raw)
+   
 #Read from an online page    
 def read_from_web():   
     #url= "http://www-personal.umd.umich.edu/~bmaxim/"
@@ -74,6 +91,10 @@ def read_from_web():
     from bs4 import BeautifulSoup
     raw= BeautifulSoup(html).get_text()
     tokens = word_tokenize(raw)
+
+# Not working, since theres no periods, it uses 
+    #sents = sent_tokenize(raw)
+    #print(sents)
     
     #Pass a variable by reference
     print_tokenizer(raw)
@@ -84,6 +105,11 @@ def lexical_diversity(my_text_data):
     vocab_size = len(set(my_text_data))
     diversity_score = vocab_size / word_count
     return diversity_score
+
+
+#def print_sentence(raw):
+#    print(sent_tokenize(raw))
+    #new_file.close()
 
 def print_tokenizer(raw):
     # Using the tokenizer library and assigning it to a variable
