490027b0d95160f78cdd3d257b7461bb64ce81b3
Marco <mfseman@umich.edu>
2019-07-10 18:25:31 -0400

    Refactored GUI page

--- NLTK_Features/GUI.py
+++ NLTK_Features/GUI.py
@@ -1,133 +1,40 @@
 from NLTK_Features.Sentiment import *
+import tkinter
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+import tkinter.scrolledtext as tkst
+from tkinter.scrolledtext import ScrolledText
 
-def Stats_Menu_Gui(window2):
-    window2.withdraw()
-
-    #Initialize third frame (analytucs menu gui)
-    window3 = Tk()
-    window3.geometry("1200x500")
-    window3.title("ANALYTICS Menu")
-
-    #Freq
-    label = Label(window3, text="Choose an option from the ANALYTICS menu: ",font=("Times New Roman Bold Italic", 40),foreground= "green")
-    label.grid(column=0, row=0)    
-
-    def local():
-           freq_Dist()
-                      
-    #LFrequency Distribution
-    option1Button = Button(window3, text= "Frequency Distribution", bg="light blue", fg="black", command = local)
-    option1Button.config(height = 1, width = 20)
-    option1Button.grid(pady=15)
-
-    def local2():
-        stop_words()
-        
-    #Stop Words
-    option2Button = Button(window3, text= "Stop Words", bg="light blue", fg="black", command = local2)
-    option2Button.config(height = 1, width = 20)
-    option2Button.grid(pady=15)
-
-    def local3():
-        stemming()
-        
-    option3Button = Button(window3, text = "Stemming", bg = "light blue", fg = "black", command = local3)
-    option3Button.config(height = 1, width = 20)
-    option3Button.grid(pady = 15)
-
-    def local4():
-        sentiment_analysis()
-    option4Button = Button(window3, text = 'Sentiment Analysis', bg = 'light blue', fg = 'black', command = local4)
-    option4Button.config(height = 1, width = 20)
-    option4Button.grid(pady = 15)
-
-    def local5():
-        sentece_word_tag()
-    option5Button = Button(window3, text = 'Word Tagging', bg = 'light blue', fg = 'black', command = local5)
-    option5Button.config(height = 1, width = 20)
-    option5Button.grid(pady = 15)
-
-    def local6():
-        Synonym_analysis()
-    option5Button = Button(window3, text = 'Synonym Analysis', bg = 'light blue', fg = 'black', command = local6)
-    option5Button.config(height = 1, width = 20)
-    option5Button.grid(pady = 15)
-    
-        
-    def quite():
-        window3.destroy()
-        window2.deiconify()
-
-    goBackBtn = Button(window3, text="Go Back",bg="light blue", fg="black", command = quite)
-    goBackBtn.grid(pady=15)   
-    window3.mainloop()
-    
-    
-#Gui for Minor menu, we pass in the old frame so we can get to it again in case we need to go back        
-def Minor_Menu_Gui(window):
-        #Hide main frame
-        window.withdraw()
-
-        #Initialize second frame (minor menu gui)
-        window2 = Tk()
-        window2.geometry("800x500")
-        window2.title("Minor Menu")
-        
-        label = Label(window2, text="Choose an option from the menu: ",font=("Times New Roman Bold Italic", 40),foreground= "green")
-        label.grid(column=0, row=0)
-
-        def local():
-           read_text()
-            
-            
-        #Local file
-        option1Button = Button(window2, text= "Access Local", bg="light blue", fg="black", command = local)
-        option1Button.config(height = 1, width = 20)
-        option1Button.grid(pady=15)
-
-        def local2():
-            read_from_web()
-            
-        #WEB
-        option2Button = Button(window2, text= "Access Web", bg="light blue", fg="black", command = local2)
-        option2Button.config(height = 1, width = 20)
-        option2Button.grid(pady=15)
-            
-        #ANALYTICS MENU
-        option3Button = Button(window2, text= "Access Analytics Menu", bg="light blue", fg="black",command=  lambda: Stats_Menu_Gui(window2))
-        option3Button.config(height = 1, width = 20)
-        option3Button.grid(pady=15)
-
-        def quite():
-            window2.destroy()
-            window.deiconify()
-            
-        goBackBtn = Button(window2, text="Go Back",bg="light blue", fg="black", command = quite)
-        goBackBtn.grid(pady=15)
-
-        
-        window2.mainloop()
-#First Menu        
+#************************* WINDOW 1 *************************       
 def gui_menu():
-    #library
-    import tkinter
-
     #define window from
     window = Tk()
-
     #name of frame
     window.title("HOME PAGE")
-
     #dimensions
-    window.geometry('1000x650+0+0')
+    window.geometry('360x170+0+0')
+    
+    # Positioning the Window
+    window.withdraw()
+    window.update_idletasks()  # Update "requested size" from geometry manager
+    x = (window.winfo_screenwidth() - window.winfo_reqwidth()) / 2
+    y = (window.winfo_screenheight() - window.winfo_reqheight()) / 2
+    window.geometry("+%d+%d" % (x-110, y-20))
+
+    # This seems to draw the window frame immediately, so only call deiconify()
+    # after setting correct window position
+    window.deiconify()
+    
+    window.configure(bg='gray86')
 
     #text (label + position)
-    lbl = Label(window, text="Welcome to UMD ANALYTICS!", font=("Times New Roman Bold Italic", 50), foreground= "blue")
+    lbl = Label(window, text="UMD ANALYTICS!", font=("Arial Bold Italic", 30), foreground= "dodgerblue3", bg = 'gray86')
     lbl.grid(column=0, row=0)
 
     #Access File button
     #opens other Minor Menu GUI, we pass the current GUI (window), so as to open it later on through hide & show 
-    option1Button = Button(window, text= "Access Corpora", bg="light blue", fg="black", command=  lambda: Minor_Menu_Gui(window))
+    option1Button = Button(window, text= "Access Corpora", bg="skyblue2", fg="black", command=  lambda: Minor_Menu_Gui(window))
     option1Button.grid(pady=15)
 
     #Message Box (confirm exit)
@@ -145,18 +52,73 @@ def gui_menu():
        os.system("taskkill /f /im pythonw.exe")
     
     #Exit Program Button
-    option2Button = Button(window, text= "Exit Program     ", bg="light blue", fg="black",command=message_Box)
+    option2Button = Button(window, text= "Exit Program     ", bg="skyblue2", fg="black", command=message_Box)
     option2Button.grid(pady=15)    
     
     #start window frame
     window.mainloop()
+#******************************END WINDOW 1****************************
 
+    
+#Gui for Minor menu, we pass in the old frame so we can get to it again in case we need to go back
 
-import tkinter as tk
-from tkinter import *
-from tkinter import ttk
+### ***************************WINDOW #2*********************************
+    
+def Minor_Menu_Gui(window):
+        #Hide main frame
+        window.withdraw()
+        
+        #Initialize second frame (minor menu gui)
+        window2 = Tk()
+        window2.geometry("430x280")
+        window2.title("Minor Menu")
+        window2.configure(bg='gray86')
+        
+        # Positioning the Window
+        window2.update_idletasks()  # Update "requested size" from geometry manager
+        x = (window2.winfo_screenwidth() - window2.winfo_reqwidth()) / 2
+        y = (window2.winfo_screenheight() - window2.winfo_reqheight()) / 2
+        window2.geometry("+%d+%d" % (x-150, y-80))
 
-import tkinter.scrolledtext as tkst
+        # This seems to draw the window frame immediately, so only call deiconify()
+        # after setting correct window position
+        window2.deiconify()
+        
+        label = Label(window2, text=" Select From Option Menu: ",font=("Arial Bold Italic", 24),foreground= "dodgerblue3", bg = 'gray86')
+        label.grid(column=0, row=0)
+
+        def local():
+           read_text()
+            
+            
+        #Local file
+        option1Button = Button(window2, text= "Parse Local File", bg="skyblue2", fg="black", command = local)
+        option1Button.config(height = 1, width = 20)
+        option1Button.grid(pady=15)
+
+        def local2():
+            read_from_web()
+            
+        #WEB
+        option2Button = Button(window2, text= "Parse Website", bg="skyblue2", fg="black", command = local2)
+        option2Button.config(height = 1, width = 20)
+        option2Button.grid(pady=15)
+            
+        #ANALYTICS MENU
+        option3Button = Button(window2, text= "Access Analytics Menu", bg="skyblue2", fg="black",command=  lambda: Stats_Menu_Gui(window2))
+        option3Button.config(height = 1, width = 20)
+        option3Button.grid(pady=15)
+
+        def quite():
+            window2.destroy()
+            window.deiconify()
+            
+        goBackBtn = Button(window2, text="Go Back",bg="skyblue2", fg="black", command = quite)
+        goBackBtn.grid(pady=15)
+
+        
+        window2.mainloop()
+#******************************** END OF WINDOW 2 *************************
 
 def createWidgets():
     
@@ -187,9 +149,6 @@ def createWidgets():
     
     win.mainloop()
     
-from tkinter import ttk
-import tkinter as tk
-from tkinter.scrolledtext import ScrolledText
 def demo():
     root = tk.Tk()
     root.title("ttk.Notebook")
@@ -216,4 +175,79 @@ def demo():
 
     nb.pack(expand=1, fill="both")
     
-    root.mainloop()      
+    root.mainloop()
+
+### ************************* WINDOW 3 *****************************************
+def Stats_Menu_Gui(window2):
+    window2.withdraw()
+
+    #Initialize third frame (analytucs menu gui)
+    window3 = Tk()
+    window3.geometry("350x500")
+    window3.title("ANALYTICS Menu")
+
+    # Positioning the Window
+    window3.update_idletasks()  # Update "requested size" from geometry manager
+    x = (window3.winfo_screenwidth() - window3.winfo_reqwidth()) / 2
+    y = (window3.winfo_screenheight() - window3.winfo_reqheight()) / 2
+    window3.geometry("+%d+%d" % (x-100, y-150))
+
+    # This seems to draw the window frame immediately, so only call deiconify()
+    # after setting correct window position
+    window3.deiconify()
+
+    #Freq
+    label = Label(window3, text=" ANALYTICS MENU: ",font=("Times New Roman Bold Italic", 26),foreground= "dodgerblue3", bg='gray86')
+    window3.configure(bg='gray86')
+    label.grid(column=0, row=0)
+
+    def local():
+        sentece_word_tag()
+    option5Button = Button(window3, text = 'Word Tagging', bg = 'skyblue2', fg = 'black', command = local)
+    option5Button.config(height = 1, width = 20)
+    option5Button.grid(pady = 15)
+
+    def local2():
+        stop_words()
+        
+    #Stop Words
+    option2Button = Button(window3, text= "Stop Words", bg="skyblue2", fg="black", command = local2)
+    option2Button.config(height = 1, width = 20)
+    option2Button.grid(pady=15)
+
+    def local3():
+        stemming()
+        
+    option3Button = Button(window3, text = "Stemming", bg = "skyblue2", fg = "black", command = local3)
+    option3Button.config(height = 1, width = 20)
+    option3Button.grid(pady = 15)
+
+    def local4():
+        sentiment_analysis()
+    option4Button = Button(window3, text = 'Sentiment Analysis', bg = 'skyblue2', fg = 'black', command = local4)
+    option4Button.config(height = 1, width = 20)
+    option4Button.grid(pady = 15)
+
+    def local5():
+        freq_Dist()
+                      
+    #LFrequency Distribution
+    option1Button = Button(window3, text= "Frequency Distribution", bg="skyblue2", fg="black", command = local5)
+    option1Button.config(height = 1, width = 20)
+    option1Button.grid(pady=15)
+
+    def local6():
+        Synonym_analysis()
+    option5Button = Button(window3, text = 'Synonym Analysis', bg = 'skyblue2', fg = 'black', command = local6)
+    option5Button.config(height = 1, width = 20)
+    option5Button.grid(pady = 15)
+        
+    def quite():
+        window3.destroy()
+        window2.deiconify()
+
+    goBackBtn = Button(window3, text="Go Back",bg="skyblue2", fg="black", command = quite)
+    goBackBtn.grid(pady=15)
+    
+    window3.mainloop()
+#********************************* END OF WINDOW 3 **************************
